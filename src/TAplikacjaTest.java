05b0e79db10a192209aeb23603541375b8890ee0
Unknown <226014@student.pwr.edu.pl>
2018-01-12 11:20:20 +0100

    added list junit parameters and setuo method

--- src/TAplikacjaTest.java
+++ src/TAplikacjaTest.java
@@ -1,8 +1,31 @@
+import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+
 public class TAplikacjaTest {
+
+    @Parameter
+    public List<TKlient> listaKlient = new ArrayList<>();
+
+    @Parameter
+    private List<TPracownik> listaPracownik = new ArrayList<>();
+
+    @Before
+    public void setUp(){
+        listaPracownik.add(new TPracownik(1, "Pawel", "Kowalski"));
+        listaKlient.add(new TKlient(1, "Jan", "Klocek", 981108023, "ul. Kobry 11", 99500, "Szlaufy", "456-980-123", "APR123456"));
+    }
+
     @Test
     public void przelejSrodki() throws Exception {
     }
