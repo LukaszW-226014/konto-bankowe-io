090c452ce66b5f3b9c87946f13544ad005c408d8
Unknown <226014@student.pwr.edu.pl>
2018-01-18 15:57:39 +0100

    add fitnesse test class

---
+++ src/FitnesseTest.java
@@ -0,0 +1,23 @@
+public class FitnesseTest {
+    float kwota;
+    float saldo;
+    boolean result;
+
+    public void setKwota(float kwota) {
+        this.kwota = kwota;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public void execute(){
+        TKonto konto = new TKonto(saldo);
+        TAplikacja app = new TAplikacja();
+        this.result = app.sprawdzSaldo(kwota, konto);
+    }
+
+    public boolean result(){
+        return result;
+    }
+}
